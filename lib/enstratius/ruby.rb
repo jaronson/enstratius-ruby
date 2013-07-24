435f4e23737606611df1505c544acb9d1f8093ce
Josh Aronson <jparonson@gmail.com>
2013-07-24 15:00:27 -0500

    Initial commit

---
+++ lib/enstratius/ruby.rb
@@ -0,0 +1,7 @@
+require "enstratius/ruby/version"
+
+module Enstratius
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/enstratius/ruby/version.rb
@@ -0,0 +1,5 @@
+module Enstratius
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
